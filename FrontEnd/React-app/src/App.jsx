fa5e8d8823dfd9da29c936a9168ac49e050cb3d7
Majdi Benboubaker <majdiboubaker82@gmail.com>
2024-02-17 21:36:33 +0100

    SideNavbar

--- FrontEnd/React-app/src/App.jsx
+++ FrontEnd/React-app/src/App.jsx
@@ -1,14 +1,13 @@
 import { SideNavbar } from "./SharedComponents/SideNavbar"
-import { Accueil } from "./SharedPages/Accueil"
-import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
       <SideNavbar />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={ <Accueil />} />
-      </Routes>
+      </Routes> */}
 
     </div>
   )
