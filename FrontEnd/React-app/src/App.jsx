ffb68103df9ced6dd2de7824c6265ce4fcb0b7c7
AmirBoujelben <aboujelben9@gmail.com>
2024-02-20 17:13:35 +0100

    created Login page/form

--- FrontEnd/React-app/src/App.jsx
+++ FrontEnd/React-app/src/App.jsx
@@ -1,15 +1,16 @@
 import { SideNavbar } from "./SharedComponents/SideNavbar"
 import { Accueil } from "./SharedPages/Accueil"
+import LoginPage from "./SharedPages/LoginPage";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <SideNavbar />
+      
       <Routes>
         <Route path="/" element={ <Accueil />} />
+        <Route path="/Login" element={ <LoginPage />} />
       </Routes>
-
     </div>
   )
 }
